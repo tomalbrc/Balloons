6776b02a5e7304af6129cecef9b7ae73cf9b8600
tomalbrc <tomalbrc@me.com>
2025-05-30 18:59:08 +0200

    Initial commit

---
+++ src/main/java/de/tomalbrc/balloons/Balloons.java
@@ -0,0 +1,10 @@
+package de.tomalbrc.balloons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Balloons implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
